f7a30b729e18f45e4a7ec857b2613f0f21755f6c
Estherbyamasu <estherbyamasufuraha@gmail.com>
2021-05-26 03:00:27 -0700

    add controller cas_prisonnier and model

---
+++ app/Http/Controllers/Cas_prisonniersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cas_prisonnier;
+use Illuminate\Http\Request;
+
+class Cas_prisonniersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cas_prisonnier  $cas_prisonnier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cas_prisonnier $cas_prisonnier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cas_prisonnier  $cas_prisonnier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cas_prisonnier $cas_prisonnier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cas_prisonnier  $cas_prisonnier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cas_prisonnier $cas_prisonnier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cas_prisonnier  $cas_prisonnier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cas_prisonnier $cas_prisonnier)
+    {
+        //
+    }
+}
